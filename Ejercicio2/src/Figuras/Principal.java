dcd674e7e6b8a34de542a9cd59fcb3bc35812f83
Sebastian Burgos <sburgosd@unal.edu.co>
2024-09-10 20:33:17 -0500

    second

---
+++ Ejercicio2/src/Figuras/Principal.java
@@ -0,0 +1,10 @@
+package Figuras;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
